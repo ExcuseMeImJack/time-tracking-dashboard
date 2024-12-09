3de41d99e42cefea9b4315cc1f227c5663aaef6a
ExcuseMeImJack <ExcuseMeImJack@gmail.com>
2024-12-09 15:54:45 -0800

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,67 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {/* Report for
+      Jeremy Robson
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      Daily
+      Weekly
+      Monthly
+
+      Work
+      5hrs <!-- daily -->
+      Previous - 7hrs <!-- daily -->
+      32hrs <!-- weekly -->
+      Previous - 36hrs <!-- weekly -->
+      103hrs <!-- monthly -->
+      Previous - 128hrs <!-- monthly -->
+
+      Play
+      1hr <!-- daily -->
+      Previous - 2hrs <!-- daily -->
+      10hrs <!-- weekly -->
+      Previous - 8hrs <!-- weekly -->
+      23hrs <!-- monthly -->
+      Previous - 29hrs <!-- monthly -->
+
+      Study
+      0hrs <!-- daily -->
+      Previous - 1hr <!-- daily -->
+      4hrs <!-- weekly -->
+      Previous - 7hrs <!-- weekly -->
+      13hrs <!-- monthly -->
+      Previous - 19hrs <!-- monthly -->
+
+      Exercise
+      1hr <!-- daily -->
+      Previous - 1hr <!-- daily -->
+      4hrs <!-- weekly -->
+      Previous - 5hrs <!-- weekly -->
+      11hrs <!-- monthly -->
+      Previous - 18hrs <!-- monthly -->
+
+      Social
+      1hr <!-- daily -->
+      Previous - 3hrs <!-- daily -->
+      5hrs <!-- weekly -->
+      Previous - 10hrs <!-- weekly -->
+      21hrs <!-- monthly -->
+      Previous - 23hrs <!-- monthly -->
+
+      Self Care
+      0hrs <!-- daily -->
+      Previous - 1hr <!-- daily -->
+      2hrs <!-- weekly -->
+      Previous - 2hrs <!-- weekly -->
+      7hrs <!-- monthly -->
+      Previous - 11hrs <!-- monthly -->
+ */}
+
+      <div className="attribution text-sm text-center text-[hsl(228, 45%, 44%)]">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Jack Roybal</a>.
+      </div>
     </div>
   );
 }
